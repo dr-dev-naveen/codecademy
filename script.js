4df20fba9c02fb0ad60380b89f9954ec9b258a46
Naveen <tummalacherlanaveen@gmail.com>
2024-03-17 19:47:24 +0530

    Second Commit

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Hello World");
+
+let num;
+
+
+// Array of nouns
+const nouns = ["cat", "dog", "tree", "house", "car", "book", "computer", "beach", "mountain", "pizza"];
+
+// Array of adjectives
+const adjectives = ["happy", "sad", "big", "small", "green", "red", "fast", "slow", "loud", "quiet"];
+
+// Array of verbs
+const verbs = ["runs", "jumps", "sleeps", "eats", "reads", "flies", "swims", "laughs", "cries", "dances"];
+
+function nounCount() {
+    num = Math.floor(Math.random() * 10);
+    return num;
+}
+
+nounCount();
+
+
+console.log(`${adjectives[num]} ${nouns[num]} ${verbs[num]}`);
